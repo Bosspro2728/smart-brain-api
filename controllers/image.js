7f69b02f069421cf77f4ccec0b4c01c243308e1c
Bosspro2728 <122049397+Bosspro2728@users.noreply.github.com>
2024-05-28 21:04:42 +0200

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -48,7 +48,7 @@ const handleApiCall = (req, res) =>{
 
 const handleImage = (req, res, db) =>{
 	const { id } = req.body;
-	db('users1').where('id', '=', id)
+	db('users').where('id', '=', id)
   	.increment('entries', 1)
   	.returning('entries')
   	.then(entries =>{
